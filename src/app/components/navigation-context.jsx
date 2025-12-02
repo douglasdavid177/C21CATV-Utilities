6442ed9f4c701dd10cbea4cedf137628254abfb3
David Douglas <douglasdavid177@gmail.com>
2025-12-02 16:57:08 -0500

    undo prev commit, remove window.scrollTo

--- src/app/components/navigation-context.jsx
+++ src/app/components/navigation-context.jsx
@@ -36,11 +36,11 @@ export default function NavContext({ children }) {
     //console.log("Route changed to:", pathname);
 
     scrollToTopInstant();
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "instant",
-    });
+    // window.scrollTo({
+    //   top: 0,
+    //   left: 0,
+    //   behavior: "instant",
+    // });
     setOpenMenu(false);
     // document.documentElement.scrollTo({ top: 0, behavior: "instant" });
 
